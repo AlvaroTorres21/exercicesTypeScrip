3ccf80eb41c80a3b488f55ab55f4a267dec6698e
AlvaroTorres <alvarotorres297@gmail.com>
2025-10-08 12:55:37 +0200

    feat(exercice1): Do the exercice with type

--- exercice1/exercice1_1.ts
+++ exercice1/exercice1_1.ts
@@ -7,4 +7,23 @@ type BirdType = {
   canFly: boolean;
 };
 
+const eagle: BirdType = {
+  name: "Eagle",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: true,
+};
+
+
 type DogType = BirdType & { race: string, age: number }
+
+const germanShepherd: DogType = {
+  name: "German Shepherd",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: false, // Los perros no pueden volar
+  race: "German Shepherd",
+  age: 5,
+};
