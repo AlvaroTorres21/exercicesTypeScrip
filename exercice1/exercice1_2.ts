c92f5a8b4afc85c6d20125099e125282f8e92ca6
AlvaroTorres <alvarotorres297@gmail.com>
2025-10-08 13:03:06 +0200

    feat(exercice1_2): Do the exercice with interfaces and examples

---
+++ exercice1/exercice1_2.ts
@@ -0,0 +1,33 @@
+
+interface BirdInterface {
+  name: string;
+  canEat: boolean;
+  canDrink: boolean;
+  canSleep: boolean;
+  canFly: boolean;
+}
+
+const sparrow: BirdInterface = {
+  name: "Sparrow",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: true,
+};
+
+
+interface DogInterface extends BirdInterface {
+    race: string;
+    age: number;
+}
+
+const bulldog: DogInterface = {
+  name: "Bulldog",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: false, // Los perros no pueden volar
+  race: "Bulldog",
+  age: 4,
+};
+
